0eb48e339048a580e27195ae8e50efa664a2c264
andrey31453 <andrey31453@google.com>
2024-03-14 11:27:43 +0300

    f

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,2 @@
-export const test1 = () => console.log('test1 2')
-
-export const test2 = () => console.log('test2 2')
+export const test1 = () => console.log('test1 3')
+export const test2 = () => console.log('test2 3')
